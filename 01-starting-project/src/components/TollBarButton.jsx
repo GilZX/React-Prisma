74c5582a5c291c2c2ef3c67a1bbafa61b4934941
GilZX <gilrz0101@gmail.com>
2024-06-06 23:28:56 -0600

    secondcommit

--- 01-starting-project/src/components/TollBarButton.jsx
+++ 01-starting-project/src/components/TollBarButton.jsx
@@ -2,5 +2,11 @@
 
 export default function TollBarButton({children}){
 
-    return <li><button>{children}</button></li>
+
+    function HandlerClick(){
+        console.log("Hello Wordl By React and Vite.")
+    }
+
+
+   return <li><button onClick={HandlerClick}>{children}</button></li>
 }
